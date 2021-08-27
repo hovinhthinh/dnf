d84c9b20286e6828b855025f7c326766f745ed6e
Vinh Thinh Ho <hovnh@amazon.com>
2021-08-27 13:21:09 +0200

    Minor fix.

--- src/py/sbert.py
+++ src/py/sbert.py
@@ -56,7 +56,7 @@ class ClassificationDataset(torch.utils.data.Dataset):
         self.labels = labels
 
     def __getitem__(self, idx):
-        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item = {key: val[idx].clone().detach() for key, val in self.encodings.items()}
         item['labels'] = torch.tensor(self.labels[idx])
         return item
 
