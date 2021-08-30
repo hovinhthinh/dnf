c91d3ffebf9801b9524bf217238bbc2685fe6550
Vinh Thinh Ho <hovnh@amazon.com>
2021-08-30 10:40:49 +0200

    Add code for SBERT no fine-tuning.

---
+++ src/py/exp/sbert_no_finetune.py
@@ -0,0 +1,49 @@
+import os
+
+import sbert
+from cluster import get_clustering_quality
+from data import snips
+from ufd import Pipeline
+
+output_file_path = './reports/snips_no_finetune'
+if output_file_path is not None:
+    os.makedirs(output_file_path, exist_ok=True)
+
+sbert.load('sentence-transformers/paraphrase-mpnet-base-v2')
+
+intent_data = [
+    (snips.split_by_features_AddToPlaylist(), '/intra_intent_AddToPlaylist.pdf', 'intra_intent_AddToPlaylist'),
+    (snips.split_by_features_RateBook(), '/intra_intent_RateBook.pdf', 'intra_intent_RateBook'),
+    (snips.split_by_features_BookRestaurant(), '/intra_intent_BookRestaurant.pdf', 'split_by_features_BookRestaurant'),
+    (snips.split_by_features_PlayMusic(), '/intra_intent_PlayMusic.pdf', 'intra_intent_PlayMusic'),
+    (snips.split_by_features_SearchCreativeWork(), '/intra_intent_SearchCreativeWork.pdf',
+     'intra_intent_SearchCreativeWork'),
+    (snips.split_by_features_SearchScreeningEvent(), '/intra_intent_SearchScreeningEvent.pdf',
+     'intra_intent_SearchScreeningEvent'),
+]
+
+# inter-intent
+inter_intent_data = []
+for data, output, title in intent_data:
+    for u in data:
+        inter_intent_data.append((u['text'], u['intent'], False))
+p = Pipeline(inter_intent_data)
+embeddings = p.get_embeddings()
+p.plot(precomputed_embeddings=embeddings, show_labels=True,
+       output_file_path=output_file_path + '/inter_intent.pdf', title='inter intent')
+
+sbert_clusters = p.get_pseudo_clusters(k=len(p.cluster_label_2_index_map), precomputed_embeddings=embeddings)
+print('Inter-intent clustering-metrics:', get_clustering_quality(p.get_true_clusters(), sbert_clusters))
+
+# intra-intent
+for data, output, title in intent_data:
+    snips_data = []
+    for u in data:
+        snips_data.append((u['text'], u['cluster'], False))
+    p = Pipeline(snips_data)
+    embeddings = p.get_embeddings()
+    p.plot(precomputed_embeddings=embeddings, show_labels=True, output_file_path=output_file_path + output, title=title)
+
+    sbert_clusters = p.get_pseudo_clusters(k=len(p.cluster_label_2_index_map), precomputed_embeddings=embeddings)
+    print('Inter-intent clustering-metrics -', title, ':',
+          get_clustering_quality(p.get_true_clusters(), sbert_clusters))
--- src/py/ufd.py
+++ src/py/ufd.py
@@ -1,5 +1,3 @@
-import os
-import random
 from typing import List, Tuple
 
 import matplotlib.pyplot as plt
@@ -7,10 +5,9 @@ import umap
 
 import sbert
 from cluster import cop_kmeans, get_clustering_quality
-from data import snips
 
 
-def umap_plot(embeddings, labels, show_labels=False, plot_3d=False, output_file_path=None):
+def umap_plot(embeddings, labels, title=None, show_labels=False, plot_3d=False, output_file_path=None):
     if show_labels:
         plt.rcParams["figure.figsize"] = (10, 4)
     embeddings = umap.UMAP(n_components=3 if plot_3d else 2).fit_transform(embeddings)
@@ -22,10 +19,12 @@ def umap_plot(embeddings, labels, show_labels=False, plot_3d=False, output_file_
         if plot_3d:
             ax.scatter([embeddings[i][0] for i in idx],
                        [embeddings[i][1] for i in idx],
-                       [embeddings[i][2] for i in idx], label=l, s=7)
+                       [embeddings[i][2] for i in idx], label='{} ({})'.format(l, len(idx)), s=7)
         else:
             ax.scatter([embeddings[i][0] for i in idx],
-                       [embeddings[i][1] for i in idx], label=l, s=7)
+                       [embeddings[i][1] for i in idx], label='{} ({})'.format(l, len(idx)), s=7)
+    if title is not None:
+        plt.title(title)
     if show_labels:
         plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
     plt.tight_layout()
@@ -83,75 +82,13 @@ class Pipeline(object):
         else:
             raise Exception('Method {} not supported'.format(method))
 
-    def plot(self, show_labels=False, precomputed_embeddings=None, plot_3d=False, output_file_path=None):
+    def plot(self, title=None, show_labels=False, precomputed_embeddings=None, plot_3d=False, output_file_path=None):
         umap_plot(precomputed_embeddings if precomputed_embeddings is not None else self.get_embeddings(),
-                  [u[1] for u in self.utterances], show_labels=show_labels, plot_3d=plot_3d,
+                  [u[1] for u in self.utterances], title=title, show_labels=show_labels, plot_3d=plot_3d,
                   output_file_path=output_file_path)
 
     def find_tune_pseudo_classification(self, k=None, precomputed_embeddings=None):
-        pseudo_clusters = p.get_pseudo_clusters(k=k if k is not None else len(self.cluster_label_2_index_map),
-                                                precomputed_embeddings=precomputed_embeddings)
+        pseudo_clusters = self.get_pseudo_clusters(k=k if k is not None else len(self.cluster_label_2_index_map),
+                                                   precomputed_embeddings=precomputed_embeddings)
         print('Clustering quality before fine-tuning:', get_clustering_quality(p.get_true_clusters(), pseudo_clusters))
         sbert.fine_tune_classification([u[0] for u in self.utterances], pseudo_clusters)
-
-
-if __name__ == '__main__':
-    intent_data = snips.split_by_features_GetWeather()
-    # The commented code below is to clustering only by intents
-    # intent_data.extend(snips.split_by_features_AddToPlaylist())
-    # intent_data.extend(snips.split_by_features_RateBook())
-    # intent_data.extend(snips.split_by_features_BookRestaurant())
-    # intent_data.extend(snips.split_by_features_PlayMusic())
-    # intent_data.extend(snips.split_by_features_SearchCreativeWork())
-    # intent_data.extend(snips.split_by_features_SearchScreeningEvent())
-    # for u in intent_data:
-    #     u['cluster'] = 'UNK'
-
-    # intent_data = snips.split_by_features_AddToPlaylist()
-    # intent_data = snips.split_by_features_RateBook()
-    # intent_data = snips.split_by_features_BookRestaurant()
-    # intent_data = snips.split_by_features_PlayMusic()
-    # intent_data = snips.split_by_features_SearchCreativeWork()
-    # intent_data = snips.split_by_features_SearchScreeningEvent()
-
-    intent_map = dict((n, i) for i, n in enumerate(set([u['intent'] + '_' + u['cluster'] for u in intent_data])))
-
-    sbert.load('sentence-transformers/paraphrase-mpnet-base-v2')
-
-    # Prepare data
-    snips_data = []
-
-    clusters = list(set([u['cluster'] for u in intent_data]))
-    random.shuffle(clusters)
-    train_clusters = clusters[0:int(len(clusters) * 0.6)]
-    for u in intent_data:
-        is_train = u['cluster'] in train_clusters
-        # is_train = u['cluster'] in ['GetCurrentWeatherInALocation', 'GetWeatherInCurrentPositionAtATimeRange']
-        snips_data.append((u['text'], u['intent'] + '_' + u['cluster'] + ('_T' if is_train else ''), is_train))
-
-    p = Pipeline(snips_data)
-
-    # Pseudo clustering
-    embeddings = p.get_embeddings()
-
-    output_file_path = './reports/fine_tune_pseudo_classification'
-    # output_file_path = None
-
-    if output_file_path is not None:
-        os.makedirs(output_file_path, exist_ok=True)
-    p.plot(show_labels=True, precomputed_embeddings=embeddings, plot_3d=False,
-           output_file_path=output_file_path + '/0.pdf' if output_file_path is not None else None)
-    for iter in range(10):
-        print('Iter: #{}'.format(iter + 1))
-        p.find_tune_pseudo_classification(precomputed_embeddings=embeddings)
-        embeddings = p.get_embeddings()
-        p.plot(show_labels=True, precomputed_embeddings=embeddings, plot_3d=False,
-               output_file_path=output_file_path + '/{}.pdf'.format(iter + 1)
-               if output_file_path is not None else None)
-
-    sbert_clusters = p.get_pseudo_clusters(k=len(p.cluster_label_2_index_map), precomputed_embeddings=embeddings)
-    print('Clustering quality after fine-tuning:', get_clustering_quality(p.get_true_clusters(), sbert_clusters))
-
-    sbert.save('test_model')
-
-    pass
