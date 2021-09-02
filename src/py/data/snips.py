9863ab6e2fe5b3553f350cdf890fc612e29a5560
Vinh Thinh Ho <hovnh@amazon.com>
2021-09-02 14:30:12 +0200

    Update splitting SNIPS into train/dev/test.

--- src/py/data/snips.py
+++ src/py/data/snips.py
@@ -712,83 +712,132 @@ def split_by_features_SearchScreeningEvent(output_file=None):
     )
 
 
-def get_train_test_data(generate_data=False):
+def get_train_test_data(generate_data=False, use_dev=True):
     """
-    :return: intra_intent_data: List[Tuple[utterance: str, cluster_label: any, is_train: bool]],
+    :return: intra_intent_data: List[Tuple[utterance: str, cluster_label: any, sample_type: 'TRAIN'|'DEV'|'TEST']],
     inter_intent_data: List[Tuple[intent_name, List]]
     """
-    train_test_data_file = './data/snips/slot_based_clusters/train_test_data.json'
+    train_test_data_file = './data/snips/slot_based_clusters/train_test_data_global.json'
 
     if generate_data:
         intent_data = [
-            split_by_features_GetWeather(),
-            split_by_features_AddToPlaylist(),
-            split_by_features_RateBook(),
-            split_by_features_BookRestaurant(),
-            split_by_features_PlayMusic(),
-            split_by_features_SearchCreativeWork(),
-            split_by_features_SearchScreeningEvent()
+            # (split_by_features_GetWeather(), { # Hard-code train/dev/test clusters
+            #     'TRAIN': ['GetWeatherInCurrentPositionAtATimeRange'],
+            #     'DEV': ['GetCurrentWeatherInCurrentPosition'],
+            #     'TEST': ['GetWeatherInALocationAtATimeRange', 'GetCurrentWeatherInALocation'],
+            # }),
+            (split_by_features_GetWeather(), None),
+            (split_by_features_AddToPlaylist(), None),
+            (split_by_features_RateBook(), None),
+            (split_by_features_BookRestaurant(), None),
+            (split_by_features_PlayMusic(), None),
+            (split_by_features_SearchCreativeWork(), None),
+            (split_by_features_SearchScreeningEvent(), None),
         ]
 
-        inter_intent_data = []
         intra_intent_data = []
         # Prepare data
-        for data in intent_data:
+        for data, predefined_clusters in intent_data:
             clusters = list(set([u['cluster'] for u in data]))
-            # Filter clusters with small size
-            clusters = [c for c in clusters if sum([1 for u in data if u['cluster'] == c]) >= 40]
 
-            # Random train clusters
-            random.shuffle(clusters)
-            train_clusters = clusters[0:int(len(clusters) * 0.6)]
+            if predefined_clusters is not None:
+                train_clusters = predefined_clusters['TRAIN'],
+                dev_clusters = predefined_clusters['DEV'],
+                test_clusters = predefined_clusters['TEST']
+                for c in train_clusters + dev_clusters + test_clusters:
+                    if c not in clusters:
+                        raise Exception('Invalid cluster:', c)
+            else:
+                # Filter clusters with small size
+                clusters = [c for c in clusters if sum([1 for u in data if u['cluster'] == c]) >= 50 and c != 'UNK']
+
+                # Random train/dev/test clusters
+                random.shuffle(clusters)
+                train_clusters = clusters[:int(len(clusters) * 0.4)]
+                dev_clusters = clusters[int(len(clusters) * 0.4):int(len(clusters) * 0.7)]
+                test_clusters = clusters[int(len(clusters) * 0.7):]
 
             # Split into train/test utterances
             splitted_data = []
             for c in clusters:
                 cluster_data = [u for u in data if u['cluster'] == c]
-
+                random.shuffle(cluster_data)
                 if c in train_clusters:
-                    random.shuffle(cluster_data)
-                    train_size = int(len(cluster_data) * 0.6)
-                    splitted_data.extend([(u['text'], c + '_T', True) for u in cluster_data[0:train_size]])
-                    splitted_data.extend([(u['text'], c + '_T', False) for u in cluster_data[train_size:]])
-                else:
-                    splitted_data.extend([(u['text'], c, False) for u in cluster_data])
+                    splitted_data.extend([(u['text'], c + '_TRAIN', 'TRAIN') for u in
+                                          cluster_data[:int(len(cluster_data) * 0.4)]])
+                    splitted_data.extend([(u['text'], c + '_TRAIN', 'DEV') for u in
+                                          cluster_data[int(len(cluster_data) * 0.4):int(len(cluster_data) * 0.7)]])
+                    splitted_data.extend([(u['text'], c + '_TRAIN', 'TEST') for u in
+                                          cluster_data[int(len(cluster_data) * 0.7):]])
+                elif c in dev_clusters:
+                    splitted_data.extend([(u['text'], c + '_DEV', 'DEV') for u in
+                                          cluster_data[:int(len(cluster_data) * 0.7)]])
+                    splitted_data.extend([(u['text'], c + '_DEV', 'TEST') for u in
+                                          cluster_data[int(len(cluster_data) * 0.7):]])
+                elif c in test_clusters:
+                    splitted_data.extend([(u['text'], c + '_TEST', 'TEST') for u in cluster_data])
 
             intra_intent_data.append((data[0]['intent'], splitted_data))
-            inter_intent_data.extend(
-                [(text, data[0]['intent'] + '_' + cluster, is_train) for text, cluster, is_train in splitted_data])
 
-        # with open(train_test_data_file, 'w') as f:
-        #     f.write(json.dumps((intra_intent_data, inter_intent_data)))
+        with open(train_test_data_file, 'w') as f:
+            f.write(json.dumps(intra_intent_data))
     else:
-        intra_intent_data, inter_intent_data = json.loads(open(train_test_data_file).read())
+        intra_intent_data = json.loads(open(train_test_data_file).read())
+
+    # Merge DEV into TRAIN if dev is not used.
+    if not use_dev:
+        for intent_name, cluster_data in intra_intent_data:
+            for u in cluster_data:
+                if u[1].endswith('_DEV'):
+                    u[1] = u[1][:-4] + '_TRAIN'
+                if u[2] == 'DEV':
+                    u[2] = 'TRAIN'
+
+    inter_intent_data = []
+    for intent_name, cluster_data in intra_intent_data:
+        inter_intent_data.extend(
+            [(text, intent_name + '_' + cluster, sample_type) for text, cluster, sample_type in cluster_data])
 
     print('======== Cluster information ========')
-    for name, cluster_data in intra_intent_data:
-        print("====", name)
+    intra_intent_data_filtered = []
+    for intent_name, cluster_data in intra_intent_data:
+        print("====", intent_name)
         clusters = set(u[1] for u in cluster_data)
         train_clusters = []
+        dev_clusters = []
         test_clusters = []
         for c in clusters:
-            if c.endswith('_T'):
-                train_clusters.append(c)
-            else:
-                test_clusters.append(c)
+            total = len([u for u in cluster_data if u[1] == c])
+            if c.endswith('_TRAIN'):
+                train_clusters.append((c, total))
+            elif c.endswith('_DEV'):
+                dev_clusters.append((c, total))
+            elif c.endswith('_TEST'):
+                test_clusters.append((c, total))
+
         print('Train:', train_clusters)
+        if use_dev:
+            print('Dev:', dev_clusters)
         print('Test:', test_clusters)
 
-    return intra_intent_data, inter_intent_data
+        if len(train_clusters) < 2 or len(test_clusters) < 2 or (use_dev and len(dev_clusters) < 2):
+            print('Ignore this intent for intra-intent setting')
+        else:
+            intra_intent_data_filtered.append((intent_name, cluster_data))
+
+    return intra_intent_data_filtered, inter_intent_data
+
+
+def print_train_dev_test_stats(intent_data):
+    train_size = len([u for u in intent_data if u[2] == 'TRAIN'])
+    dev_size = len([u for u in intent_data if u[2] == 'DEV'])
+    test_size = len([u for u in intent_data if u[2] == 'TEST'])
+    print('Train/Dev/Test utterances: {}/{}/{} ({:.1f}%,{:.1f}%,{:.1f}%)'.format(train_size, dev_size, test_size,
+                                                                                 100 * train_size / len(intent_data),
+                                                                                 100 * dev_size / len(intent_data),
+                                                                                 100 * test_size / len(intent_data)))
 
 
 if __name__ == '__main__':
     get_train_test_data()
-    split_by_features_AddToPlaylist('data/snips/slot_based_clusters/AddToPlaylist.json')
-    split_by_features_BookRestaurant('data/snips/slot_based_clusters/BookRestaurant.json')
-    split_by_features_GetWeather('data/snips/slot_based_clusters/GetWeather.json')
-    split_by_features_RateBook('data/snips/slot_based_clusters/RateBook.json')
-    split_by_features_PlayMusic('data/snips/slot_based_clusters/PlayMusic.json')
-    split_by_features_SearchCreativeWork('data/snips/slot_based_clusters/SearchCreativeWork.json')
-    split_by_features_SearchScreeningEvent('data/snips/slot_based_clusters/SearchScreeningEvent.json')
-
     pass
