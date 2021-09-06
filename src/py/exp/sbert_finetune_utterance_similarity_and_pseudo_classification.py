4511478725c5788234109496b9cbb9cd3ebef8ba
Vinh Thinh Ho <hovnh@amazon.com>
2021-09-06 15:00:54 +0200

    Minor update.

---
+++ src/py/exp/sbert_finetune_utterance_similarity_and_pseudo_classification.py
@@ -0,0 +1,51 @@
+import os
+
+from data import snips
+from data.snips import print_train_dev_test_stats
+from ufd import Pipeline
+
+report_folder = './reports/global/snips_finetune_utterance_similarity_and_pseudo_classification/'
+
+intra_intent_data, inter_intent_data = snips.get_train_test_data(use_dev=True)
+
+pipeline_steps = [
+    'no-finetune',
+    'finetune-utterance-similarity',
+    'finetune-pseudo-classification',
+]
+
+# Processing intra-intents
+for intent_name, intent_data in intra_intent_data:
+    print('======================================== Intra-intent:', intent_name,
+          '========================================')
+    print_train_dev_test_stats(intent_data)
+    p = Pipeline(intent_data, dataset_name=intent_name)
+    intent_report_folder = os.path.join(report_folder, intent_name) if report_folder is not None else None
+    p.run(report_folder=intent_report_folder, steps=pipeline_steps, config={'classification_sample_weights': True})
+
+# Processing inter-intents
+print('======================================== Inter-intent ======================================')
+print_train_dev_test_stats(inter_intent_data)
+p = Pipeline(inter_intent_data, dataset_name='inter-intent')
+intent_report_folder = os.path.join(report_folder, 'inter_intent') if report_folder is not None else None
+p.run(report_folder=intent_report_folder, steps=pipeline_steps, config={'classification_sample_weights': True})
+
+# Apply back to intra-intent
+print('======== Apply inter-intent model back to intra-intent ========')
+
+stats_file = None
+if report_folder is not None:
+    os.makedirs(report_folder, exist_ok=True)
+    stats_file = open(os.path.join(report_folder, 'stats.txt'), 'w')
+    stats_file.write('======== Apply inter-intent model back to intra-intent ========\n')
+
+for intent_name, intent_data in intra_intent_data:
+    p = Pipeline(intent_data)
+    p.update_test_embeddings()
+    test_quality = p.get_test_clustering_quality()
+    print('Clustering test quality [{}]: {}'.format(intent_name, test_quality))
+    if stats_file is not None:
+        stats_file.write('Clustering test quality [{}]: {}\n'.format(intent_name, test_quality))
+
+if stats_file is not None:
+    stats_file.close()
