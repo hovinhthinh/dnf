bb79832a5c475c6ab377a157114b2b575ee39e65
Vinh Thinh Ho <hovnh@amazon.com>
2021-09-08 00:21:31 +0200

    Code for fine-tuning with slot recognition.

--- src/py/sbert.py
+++ src/py/sbert.py
@@ -18,7 +18,7 @@ model = None
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def load(model_path='sentence-transformers/paraphrase-mpnet-base-v2', from_tf=False):
+def load(model_path='./models/sentence-transformers.paraphrase-mpnet-base-v2', from_tf=False):
     global tokenizer, model
     tokenizer = AutoTokenizer.from_pretrained(model_path, from_tf=from_tf)
     model = AutoModel.from_pretrained(model_path, from_tf=from_tf)
@@ -355,14 +355,231 @@ def fine_tune_utterance_similarity(train_texts, train_labels,
         print('Evaluate test_dataset (after):', trainer.evaluate(test_dataset))
 
 
+class SlotRecognitionDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+
+
+class TaggerHead(nn.Module):
+    """Head for token-level classification tasks."""
+
+    def __init__(self, base_model_config, num_labels):
+        super().__init__()
+        # self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+        self.dropout = nn.Dropout(base_model_config.hidden_dropout_prob)
+        self.out_proj = nn.Linear(base_model_config.hidden_size, num_labels)
+
+        print('Initializing tagger head')
+        self.out_proj.weight.data.normal_(mean=0.0, std=0.02)
+        if self.out_proj.bias is not None:
+            self.out_proj.bias.data.zero_()
+
+    def forward(self, base_output, **kwargs):
+        x = self.dropout(base_output)
+        x = self.out_proj(x)
+        return x
+
+
+class SlotRecognitionModel(nn.Module):
+
+    def __init__(self, base_model, num_labels):
+        super().__init__()
+        self.config = base_model.config
+        self.num_labels = num_labels
+        self.base_model = base_model
+        self.tagger = TaggerHead(base_model.config, num_labels)
+        self.loss_fct = CrossEntropyLoss()
+
+    def forward(
+            self,
+            input_ids=None,
+            attention_mask=None,
+            token_type_ids=None,
+            position_ids=None,
+            head_mask=None,
+            inputs_embeds=None,
+            labels=None,
+            output_attentions=None,
+            output_hidden_states=None,
+            return_dict=None,
+    ):
+        r"""
+        labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size, sequence_length)`, `optional`):
+            Labels for computing the token classification loss. Indices should be in ``[0, ..., config.num_labels -
+            1]``.
+        """
+        outputs = self.base_model(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        sequence_output = outputs[0]
+
+        logits = self.tagger(sequence_output)
+
+        # Only keep active parts of the loss
+        active_loss = attention_mask.view(-1) == 1
+        active_logits = logits.view(-1, self.num_labels)
+        active_labels = torch.where(
+            active_loss, labels.view(-1), torch.tensor(self.loss_fct.ignore_index).type_as(labels)
+        )
+        loss = self.loss_fct(active_logits, active_labels)
+
+        output = (logits,) + outputs[2:]
+        return ((loss,) + output) if loss is not None else output
+
+
+def fine_tune_slot_recognition(train_texts, train_slots,
+                               val_texts=None, val_slots=None, test_texts=None, test_slots=None,
+                               intent_name=None, n_train_epochs=-1, n_train_steps=-1):
+    def split_text_and_slots_into_tokens_and_tags(texts, slots):
+        texts = [text.split(' ') for text in texts]
+
+        tags = []
+
+        for i, tokens in enumerate(texts):
+            last_b = None
+            u_tags = []
+            cur = 0
+            for j, t in enumerate(tokens):
+                tag = None
+                if j > 0:
+                    cur += 1
+                for slot, info in slots[i].items():
+                    if cur == info['start']:
+                        assert cur + len(t) <= info['end']
+                        assert tag is None
+                        tag = ('' if intent_name is None else intent_name + '_') + 'B_' + slot
+                        last_b = tag
+                    elif info['start'] < cur < info['end']:
+                        assert cur + len(t) <= info['end']
+                        assert tag is None
+                        assert last_b == ('' if intent_name is None else intent_name + '_') + 'B_' + slot
+                        tag = ('' if intent_name is None else intent_name + '_') + 'I_' + slot
+
+                if tag is None:
+                    last_b = None
+                u_tags.append('O' if tag is None else tag)
+                cur += len(t)
+
+            tags.append(u_tags)
+
+        return texts, tags
+
+    def encode_tags(tags, encodings):
+        labels = [[tag2id[tag] for tag in doc] for doc in tags]
+        encoded_labels = []
+        for doc_labels, doc_offset in zip(labels, encodings.offset_mapping):
+            # create an empty array of -100
+            doc_enc_labels = numpy.ones(len(doc_offset), dtype=int) * -100
+            arr_offset = numpy.array(doc_offset)
+
+            # set labels whose first offset position is 0 and the second is not 0
+            doc_enc_labels[(arr_offset[:, 0] == 0) & (arr_offset[:, 1] != 0)] = doc_labels
+            encoded_labels.append(doc_enc_labels.tolist())
+
+        return encoded_labels
+
+    train_texts, train_tags = split_text_and_slots_into_tokens_and_tags(train_texts, train_slots)
+
+    if val_texts is not None:
+        val_texts, val_tags = split_text_and_slots_into_tokens_and_tags(val_texts, val_slots)
+    if test_texts is not None:
+        test_texts, test_tags = split_text_and_slots_into_tokens_and_tags(test_texts, test_slots)
+
+    # Tag set
+    unique_tags = set(tag for doc in train_tags for tag in doc)
+    if val_texts is not None:
+        unique_tags.update(tag for doc in val_tags for tag in doc)
+    if test_texts is not None:
+        unique_tags.update(tag for doc in test_tags for tag in doc)
+    tag2id = {tag: id for id, tag in enumerate(unique_tags)}
+    id2tag = {id: tag for tag, id in tag2id.items()}
+
+    train_encodings = tokenizer(train_texts, is_split_into_words=True, return_offsets_mapping=True, padding=True,
+                                truncation=True, return_tensors='pt')
+    val_encodings = tokenizer(val_texts, is_split_into_words=True, return_offsets_mapping=True, padding=True,
+                              truncation=True, return_tensors='pt') if val_texts is not None else None
+    test_encodings = tokenizer(test_texts, is_split_into_words=True, return_offsets_mapping=True, padding=True,
+                               truncation=True, return_tensors='pt') if test_texts is not None else None
+
+    train_labels = encode_tags(train_tags, train_encodings)
+    val_labels = encode_tags(val_tags, val_encodings) if val_texts is not None else None
+    test_labels = encode_tags(test_tags, test_encodings) if test_texts is not None else None
+
+    train_encodings.pop("offset_mapping")
+    if val_texts is not None:
+        val_encodings.pop("offset_mapping")
+    if test_texts is not None:
+        test_encodings.pop("offset_mapping")
+
+    train_dataset = SlotRecognitionDataset(train_encodings, train_labels)
+    val_dataset = SlotRecognitionDataset(val_encodings, val_labels) if val_texts is not None else None
+    test_dataset = SlotRecognitionDataset(test_encodings, test_labels) if test_texts is not None else None
+
+    tagger = SlotRecognitionModel(model, len(unique_tags))
+
+    if n_train_epochs == -1:
+        if n_train_steps == -1:
+            n_train_steps = 1000
+        n_train_epochs = ceil(n_train_steps / len(train_dataset))
+
+    trainer = Trainer(
+        model=tagger,
+        args=TrainingArguments(
+            save_strategy='no',
+            output_dir='./results',
+            num_train_epochs=n_train_epochs,
+            per_device_train_batch_size=16,
+            per_device_eval_batch_size=64,
+            warmup_steps=500,
+            weight_decay=0.01,
+            logging_strategy='no',
+            logging_dir='./logs',
+            evaluation_strategy='epoch' if val_dataset is not None else 'no'
+        ),
+        train_dataset=train_dataset,
+        eval_dataset=val_dataset,
+    )
+    if test_dataset is not None:
+        print('Evaluate test_dataset (before):', trainer.evaluate(test_dataset))
+
+    trainer.train()
+
+    if test_dataset is not None:
+        print('Evaluate test_dataset (after):', trainer.evaluate(test_dataset))
+
+
 if __name__ == '__main__':
     load('sentence-transformers/paraphrase-mpnet-base-v2')
 
-    train_texts, train_labels, train_labels_weights \
-        = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'], [1, 0, 1, 0, 1, 0, 1, 0], [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
-    val_texts, val_labels = ['i', 'j', 'k', 'l'], [1, 0, 1, 0]
-    fine_tune_pseudo_classification(train_texts, train_labels, train_sample_weights=train_labels_weights)
+    # train_texts, train_labels, train_labels_weights \
+    #     = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'], [1, 0, 1, 0, 1, 0, 1, 0], [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
+    # val_texts, val_labels = ['i', 'j', 'k', 'l'], [1, 0, 1, 0]
+    # fine_tune_pseudo_classification(train_texts, train_labels, train_sample_weights=train_labels_weights)
 
     # train_texts, train_labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'], [1, 0, 1, 0, 1, 0, None, None]
     # val_texts, val_labels = ['i', 'j', 'k', 'l'], [1, 0, 1, 0]
     # fine_tune_utterance_similarity(train_texts, train_labels)
+
+    # train_texts, train_slots \
+    #     = ['this is first sentence', 'this is second with oov word qwertyuiop asdfghjkl'], \
+    #       [{'slot_1': {'start': 8, 'end': 22}, 'slot_2': {'start': 0, 'end': 4}},
+    #        {'slot_1': {'start': 24, 'end': 49}, 'slot_2': {'start': 0, 'end': 4}}, ]
+    # fine_tune_slot_recognition(train_texts, train_slots, intent_name='test_intent')
