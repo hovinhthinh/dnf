0b3140cfe00fdf9b10c6fd106e435cb3c88c15e3
Vinh Thinh Ho <hovnh@amazon.com>
2021-09-13 17:06:04 +0200

    Minor fix.

--- src/py/sbert.py
+++ src/py/sbert.py
@@ -383,7 +383,7 @@ class SlotRecognitionDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         item = {key: val[idx].clone().detach() for key, val in self.encodings.items()}
-        item['labels'] = self.labels[idx]
+        item['labels'] = torch.tensor(self.labels[idx])
         return item
 
     def __len__(self):
