40da7504fe5b7ace595d3f2bf6c1e37b0c26d608
Vinh Thinh Ho <hovnh@amazon.com>
2021-09-15 13:49:43 +0200

    Code for fine-tuning slot-multiclass-classification.

--- src/py/sbert.py
+++ src/py/sbert.py
@@ -6,7 +6,7 @@ import numpy
 import torch
 from datasets import load_metric
 from torch import nn
-from torch.nn import CrossEntropyLoss, MSELoss
+from torch.nn import CrossEntropyLoss, MSELoss, BCEWithLogitsLoss
 from torch.nn.functional import cosine_similarity
 from torch.utils.data import DataLoader
 from transformers import AutoTokenizer, AutoModel, TrainingArguments, Trainer, set_seed, AdamW
@@ -60,7 +60,7 @@ def get_embeddings(utterances: List[str], batch_size=64) -> numpy.ndarray:
 
 
 class ClassificationDataset(torch.utils.data.Dataset):
-    # labels \in {0,1,2,3,...}
+    # labels \in {0,1,2,3,...} (for single-class) or tuple[Literal[0.0,1.0]] (for multi-class)
     def __init__(self, encodings, labels, sample_weights=None):
         self.encodings = encodings
         self.labels = labels
@@ -68,9 +68,9 @@ class ClassificationDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         item = {key: val[idx].clone().detach() for key, val in self.encodings.items()}
-        item['labels'] = self.labels[idx]
+        item['labels'] = torch.tensor(self.labels[idx])
         if self.sample_weights is not None:
-            item['sample_weights'] = self.sample_weights[idx]
+            item['sample_weights'] = torch.tensor(self.sample_weights[idx])
         return item
 
     def __len__(self):
@@ -78,8 +78,6 @@ class ClassificationDataset(torch.utils.data.Dataset):
 
 
 class ClassificationHead(nn.Module):
-    """Head for sentence-level classification tasks."""
-
     def __init__(self, base_model_config, num_labels):
         super().__init__()
         # self.dense = nn.Linear(config.hidden_size, config.hidden_size)
@@ -117,12 +115,6 @@ class PseudoClassificationModel(nn.Module):
             labels=None,
             sample_weights=None,
     ):
-        r"""
-        labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size,)`, `optional`):
-            Labels for computing the sequence classification/regression loss. Indices should be in :obj:`[0, ...,
-            config.num_labels - 1]`. If :obj:`config.num_labels == 1` a regression loss is computed (Mean-Square loss),
-            If :obj:`config.num_labels > 1` a classification loss is computed (Cross-Entropy).
-        """
         outputs = self.base_model(
             input_ids,
             attention_mask=attention_mask,
@@ -433,11 +425,6 @@ class SlotTaggingModel(nn.Module):
             output_hidden_states=None,
             return_dict=None,
     ):
-        r"""
-        labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size, sequence_length)`, `optional`):
-            Labels for computing the token classification loss. Indices should be in ``[0, ..., config.num_labels -
-            1]``.
-        """
         outputs = self.base_model(
             input_ids,
             attention_mask=attention_mask,
@@ -676,6 +663,93 @@ def fine_tune_joint_slot_tagging_and_utterance_similarity(train_texts, train_slo
     estimator.eval()
 
 
+class SlotMulticlassClassificationModel(nn.Module):
+    def __init__(self, base_model, num_labels):
+        super().__init__()
+        self.config = base_model.config
+        self.num_labels = num_labels
+        self.base_model = base_model
+        self.classifier = ClassificationHead(base_model.config, num_labels)
+        self.loss_fct = BCEWithLogitsLoss()
+
+    def forward(
+            self,
+            input_ids=None,
+            attention_mask=None,
+            labels=None,
+    ):
+        outputs = self.base_model(
+            input_ids,
+            attention_mask=attention_mask,
+        )
+        logits = self.classifier(_mean_pooling(outputs, attention_mask))
+
+        loss = self.loss_fct(logits, labels)
+
+        output = (logits,) + outputs[2:]
+        # The first should be loss, used by trainer. The second should be logits, used by compute_metrics
+        return (loss,) + output
+
+
+def fine_tune_slot_multiclass_classification(train_texts, train_slots,
+                                             val_texts=None, val_slots=None, test_texts=None, test_slots=None,
+                                             n_train_epochs=-1, n_train_steps=-1):
+    # Tag set
+    unique_tags = set(tag for doc in train_slots for tag in doc.keys())
+    if val_texts is not None:
+        unique_tags.update(tag for doc in val_slots for tag in doc.keys())
+    if test_texts is not None:
+        unique_tags.update(tag for doc in test_slots for tag in doc.keys())
+
+    train_encodings = tokenizer(train_texts, padding=True, truncation=True, return_tensors='pt')
+    val_encodings = tokenizer(val_texts, padding=True, truncation=True, return_tensors='pt') \
+        if val_texts is not None else None
+    test_encodings = tokenizer(test_texts, padding=True, truncation=True, return_tensors='pt') \
+        if test_texts is not None else None
+
+    train_labels = [[1.0 if s in u_slots else 0.0 for s in unique_tags] for u_slots in train_slots]
+    val_labels = [[1.0 if s in u_slots else 0.0 for s in unique_tags] for u_slots in val_slots] \
+        if val_texts is not None else None
+    test_labels = [[1.0 if s in u_slots else 0.0 for s in unique_tags] for u_slots in test_slots] \
+        if test_texts is not None else None
+
+    train_dataset = ClassificationDataset(train_encodings, train_labels)
+    val_dataset = ClassificationDataset(val_encodings, val_labels) if val_texts is not None else None
+    test_dataset = ClassificationDataset(test_encodings, test_labels) if test_texts is not None else None
+
+    classifier = SlotMulticlassClassificationModel(model, len(unique_tags))
+
+    if n_train_epochs == -1:
+        if n_train_steps == -1:
+            n_train_steps = 2000
+        n_train_epochs = max(ceil(n_train_steps / len(train_dataset)), 3)
+
+    trainer = Trainer(
+        model=classifier,
+        args=TrainingArguments(
+            save_strategy='no',
+            output_dir='./results',
+            num_train_epochs=n_train_epochs,
+            per_device_train_batch_size=16,
+            per_device_eval_batch_size=64,
+            warmup_steps=500,
+            weight_decay=0.01,
+            logging_strategy='no',
+            logging_dir='./logs',
+            evaluation_strategy='epoch' if val_dataset is not None else 'no'
+        ),
+        train_dataset=train_dataset,
+        eval_dataset=val_dataset,
+    )
+    if test_dataset is not None:
+        print('Evaluate test_dataset (before):', trainer.evaluate(test_dataset))
+
+    trainer.train()
+
+    if test_dataset is not None:
+        print('Evaluate test_dataset (after):', trainer.evaluate(test_dataset))
+
+
 if __name__ == '__main__':
     load('sentence-transformers/paraphrase-mpnet-base-v2')
 
@@ -692,11 +766,12 @@ if __name__ == '__main__':
     #     = ['this is first sentence', 'this is second with oov word qwertyuiop asdfghjkl'], \
     #       [{'slot_1': {'start': 8, 'end': 22}, 'slot_2': {'start': 0, 'end': 4}},
     #        {'slot_1': {'start': 24, 'end': 49}, 'slot_2': {'start': 0, 'end': 4}}, ]
+    # fine_tune_slot_multiclass_classification(train_texts, train_slots)
     # fine_tune_slot_tagging(train_texts, train_slots)
 
-    train_texts, train_slots, train_labels \
-        = ['this is first sentence', 'this is second with oov word qwertyuiop asdfghjkl', 'test'], \
-          [{'slot_1': {'start': 8, 'end': 22}, 'slot_2': {'start': 0, 'end': 4}},
-           {'slot_1': {'start': 24, 'end': 49}, 'slot_2': {'start': 0, 'end': 4}}], \
-          [0, 1, None]
-    fine_tune_joint_slot_tagging_and_utterance_similarity(train_texts, train_slots, train_labels)
+    # train_texts, train_slots, train_labels \
+    #     = ['this is first sentence', 'this is second with oov word qwertyuiop asdfghjkl', 'test'], \
+    #       [{'slot_1': {'start': 8, 'end': 22}, 'slot_2': {'start': 0, 'end': 4}},
+    #        {'slot_1': {'start': 24, 'end': 49}, 'slot_2': {'start': 0, 'end': 4}}], \
+    #       [0, 1, None]
+    # fine_tune_joint_slot_tagging_and_utterance_similarity(train_texts, train_slots, train_labels)
