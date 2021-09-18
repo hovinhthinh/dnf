0ebc8f0546a9b9c40993cd31981d5d56d03d5162
Vinh Thinh Ho <hovnh@amazon.com>
2021-09-18 14:15:22 +0200

    Minor update.

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
 # ./run.sh <py_module> <args...>
 args="${@:2}"
 
+export PYTHONPATH=./src/py
+export PYTHONHASHSEED=6993
+
 if [ -e "$1" ]; then
   echo 'Running script'
-  export PYTHONPATH=./src/py && python $1 $args
+  python $1 $args
 else
   echo 'Running module'
-  export PYTHONPATH=./src/py && python -m $1 $args
+  python -m $1 $args
 fi
