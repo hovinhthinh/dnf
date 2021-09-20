3edfef4f91a21ab808483f103ac0ce1ccc118664
Vinh Thinh Ho <hovnh@amazon.com>
2021-09-20 09:31:49 +0200

    Minor fix.

--- src/py/exp/sbert_finetune_SMC+US_PC.py
+++ src/py/exp/sbert_finetune_SMC+US_PC.py
@@ -1,7 +1,7 @@
 from data import snips
 from ufd import run_all_intents
 
-report_folder = './reports/global/snips_SMC+US_PS/'
+report_folder = './reports/global/snips_SMC+US_PC/'
 
 intra_intent_data, inter_intent_data = snips.get_train_test_data(use_dev=True)
 
