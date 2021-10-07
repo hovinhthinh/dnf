43f19f57f1e1c29dd415a2282e3e46d5d9487c5c
Vinh Thinh Ho <hovnh@amazon.com>
2021-10-07 11:58:45 +0200

    Add reports for experiments with integrating slot information.

---
+++ src/py/exp/sbert_finetune_SMC_US_PC_squashing_train_dev.py
@@ -0,0 +1,20 @@
+from data import snips
+from ufd import run_all_intents
+
+report_folder = './reports/global/snips_SMC_US_PC_squashing_train_dev/'
+
+intra_intent_data, inter_intent_data = snips.get_train_test_data(use_dev=True)
+
+pipeline_steps = [
+    'SMC',
+    'US',
+    'PC',
+]
+
+run_all_intents(pipeline_steps, intra_intent_data, inter_intent_data, report_folder=report_folder,
+                config={
+                    'squashing_train_dev': True,
+                    'SMC_n_train_epochs': 3,
+                    'US_n_train_epochs': 3,
+                    'PC_iterations': 3
+                })
