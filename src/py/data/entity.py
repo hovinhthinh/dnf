76451fd3a9a8c66c05b64f81b7c5da9e81e794d7
Vinh Thinh Ho <hovnh@amazon.com>
2021-10-23 14:39:20 +0200

    Minor fix.

--- src/py/data/entity.py
+++ src/py/data/entity.py
@@ -3,7 +3,7 @@ class Utterance(object):
     def __init__(self):
         self.text: str = None
         self.feature_name: str = None
-        self.feature_type: str = None # SLOT/VALUE/SLOT+VALUE/INTENT
+        self.feature_type: str = None # SLOT/SLOT+VALUE/INTENT
         self.intent_name: str = None
         self.part_type: str = None  # TRAIN/DEV/TEST
         self.slots: dict = None
