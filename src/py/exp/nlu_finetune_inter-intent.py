936c325a1932c21b68d6188088796c4516c39333
Vinh Thinh Ho <hovnh@amazon.com>
2021-11-09 10:42:13 +0100

    Minor updates.

--- src/py/exp/nlu_finetune_inter-intent.py
+++ src/py/exp/nlu_finetune_inter-intent.py
@@ -1,6 +1,8 @@
 import json
 import os
 
+from transformers import set_seed
+
 from data import snips
 from nlu import load_finetuned
 from ufd import Pipeline
@@ -10,9 +12,10 @@ report_folder = './models/snips_nlu/'
 _, inter_intent_data = snips.get_train_test_data(use_dev=True)
 
 print('Train NLU model')
+set_seed(12993)
 
 p = Pipeline(inter_intent_data, dataset_name='inter_intent')
-# p.train_nlu_model(save_model_path=os.path.join(report_folder, 'inter_intent', 'nlu_model'))
+# p.train_nlu_model(save_model_path=os.path.join(report_folder, 'inter_intent', 'nlu_model'), n_train_epochs=10)
 
 load_finetuned(os.path.join(report_folder, 'inter_intent', 'nlu_model'))
 test_quality = json.dumps(p.get_nlu_test_quality(), indent=2)
