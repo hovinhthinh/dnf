1186f67dd381e83ca0ec4450769cddb76da7d640
Vinh Thinh Ho <hovnh@amazon.com>
2021-11-15 16:09:38 +0100

    Minor updates.

--- src/py/data/entity.py
+++ src/py/data/entity.py
@@ -3,11 +3,12 @@ class Utterance(object):
     def __init__(self):
         self.text: str = None
         self.feature_name: str = None
-        self.feature_type: str = None # SLOT/SLOT+VALUE/INTENT
+        self.feature_type: str = None  # SLOT/SLOT+VALUE/INTENT
         self.intent_name: str = None
         self.part_type: str = None  # TRAIN/DEV/TEST
         self.slots: dict = None
 
+        self.domain = None  # Not available for SNIPS data
 
     def __str__(self):
         return self.__dict__.__str__()
