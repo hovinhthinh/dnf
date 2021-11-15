4b9f58326a4a93dd985bb885e83233b202708340
Vinh Thinh Ho <hovnh@amazon.com>
2021-11-15 16:30:30 +0100

    Minor updates.

--- src/py/data/entity.py
+++ src/py/data/entity.py
@@ -1,14 +1,14 @@
 class Utterance(object):
 
-    def __init__(self):
-        self.text: str = None
-        self.feature_name: str = None
-        self.feature_type: str = None  # SLOT/SLOT+VALUE/INTENT
-        self.intent_name: str = None
-        self.part_type: str = None  # TRAIN/DEV/TEST
-        self.slots: dict = None
-
-        self.domain = None  # Not available for SNIPS data
+    def __init__(self, text: str = None, feature_name: str = None, part_type: str = None, slots: dict = None,
+                 intent_name: str = None, feature_type: str = None, domain=None):
+        self.text = text
+        self.feature_name = feature_name
+        self.feature_type = feature_type  # SLOT/SLOT+VALUE/INTENT
+        self.intent_name = intent_name
+        self.part_type = part_type  # TRAIN/DEV/TEST
+        self.slots = slots
+        self.domain = domain  # Not available for SNIPS data
 
     def __str__(self):
         return self.__dict__.__str__()
