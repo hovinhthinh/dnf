9c66b2016e1d88f6c7a7014f6e65b961409f1f6a
Vinh Thinh Ho <hovnh@amazon.com>
2021-11-15 20:45:39 +0100

    Convert Alexa data format into working format (for live data).

--- src/py/data/alexa.py
+++ src/py/data/alexa.py
@@ -30,29 +30,38 @@ def _parse_text_and_slots(utr):
     return text, slot
 
 
-def read_feature_data(folder='./data/alexa/fr/frXX_feature_data/'):
+def read_utterances_from_file(file):
     utterances = []
     uniq_utrs = set()
-    for f in [f for f in os.listdir(folder)]:
-        if not os.path.isdir(os.path.join(folder, f)):
+    for line in open(file):
+        parts = line.strip().split('\t')
+
+        ucheck = '\t'.join(parts[0:3])
+        if ucheck in uniq_utrs:
             continue
-        for line in open(os.path.join(folder, f, 'feature_data_uniq.tsv')):
-            parts = line.strip().split('\t')
+        else:
+            uniq_utrs.add(ucheck)
 
-            ucheck = '\t'.join(parts[0:3])
-            if ucheck in uniq_utrs:
-                continue
-            else:
-                uniq_utrs.add(ucheck)
+        u = Utterance(domain=parts[0], intent_name=parts[1])
+        u.text, u.slots = _parse_text_and_slots(parts[2])
+        utterances.append(u)
+    return utterances
 
-            u = Utterance()
 
-            u.domain, u.intent_name = parts[0], parts[1]
+def read_feature_data(folder='./data/alexa/fr/frXX_feature_data/'):
+    utterances = []
+    for f in [f for f in os.listdir(folder)]:
+        feature_utrs = read_utterances_from_file(os.path.join(folder, f, 'feature_data_uniq.tsv'))
+        for u in feature_utrs:
             u.feature_name = f
-            u.text, u.slots = _parse_text_and_slots(parts[2])
+        utterances.extend(feature_utrs)
+    return utterances
 
-            utterances.append(u)
 
+def read_live_data(folder='./data/alexa/fr/frXX_live_traffic/'):
+    utterances = []
+    for f in [f for f in os.listdir(folder)]:
+        utterances.extend(read_utterances_from_file(os.path.join(folder, f)))
     return utterances
 
 
@@ -60,9 +69,10 @@ def get_train_test_data(generate_data=False):
     train_test_data_file = './data/alexa/fr/slot_based_clusters/train_test_data_global.json'
 
     if generate_data:
-        inter_intent_data = read_feature_data()
+        # Process feature data
+        feature_data = read_feature_data()
 
-        clusters = dict.fromkeys([u.feature_name for u in inter_intent_data])
+        clusters = dict.fromkeys([u.feature_name for u in feature_data])
         predefined_clusters = {  # TODO: pre-define TRAIN/TEST features?
             'TRAIN': [],
             'TEST': []
@@ -77,7 +87,7 @@ def get_train_test_data(generate_data=False):
                     raise Exception('Invalid cluster:', c)
         else:
             # Filter clusters with small size
-            clusters = [c for c in clusters if sum([1 for u in inter_intent_data if u.feature_name == c]) >= 100]
+            clusters = [c for c in clusters if sum([1 for u in feature_data if u.feature_name == c]) >= 100]
 
             # Random train/dev/test clusters
             random.shuffle(clusters)
@@ -89,10 +99,10 @@ def get_train_test_data(generate_data=False):
         # Split into train/test utterances
         splitted_data = []
         for c in clusters:
-            cluster_data = [u for u in inter_intent_data if u.feature_name == c]
+            cluster_data = [u for u in feature_data if u.feature_name == c]
             random.shuffle(cluster_data)
             if len(cluster_data) > 2000:  # Down sample
-                cluster_data = cluster_data[0:2000]
+                cluster_data = cluster_data[:2000]
 
             if c in train_clusters:
                 for i, u in enumerate(cluster_data):
@@ -104,43 +114,45 @@ def get_train_test_data(generate_data=False):
                     u.part_type = 'TEST'
             splitted_data.extend(cluster_data)
 
-        inter_intent_data = splitted_data
+        feature_data = splitted_data
+
+        # Process live data
+        live_data = read_live_data()
+        random.shuffle(live_data)
+        sample_size = len([u for u in feature_data if u.part_type == 'TEST'])  # Sample same size as test data
+        live_data = live_data[:sample_size]
+
+        for u in live_data:
+            u.part_type = 'DEV'
+
+        final_data = feature_data + live_data
+
         json_data = [
             [u.text, u.feature_name, u.part_type, u.slots, u.intent_name, u.domain]
-            for u in inter_intent_data
+            for u in final_data
         ]
         os.makedirs(os.path.dirname(train_test_data_file), exist_ok=True)
         with open(train_test_data_file, 'w') as f:
             f.write(json.dumps(json_data))
     else:
         json_data = json.loads(open(train_test_data_file).read())
-        inter_intent_data = [
+        final_data = [
             Utterance(text=u[0], feature_name=u[1], part_type=u[2], slots=u[3], intent_name=u[4], domain=u[5])
             for u in json_data
         ]
 
-    def get_feature_type_for_Alexa(feature_name):
-        feature_name_2_type = {
-            # TODO
-        }
-
-        if feature_name.endswith('_TRAIN'):
-            feature_name = feature_name[:-6]
-        if feature_name.endswith('_DEV'):
-            feature_name = feature_name[:-4]
-        if feature_name.endswith('_TEST'):
-            feature_name = feature_name[:-5]
-
-        return feature_name_2_type[feature_name]
-
     print('======== Cluster information ========')
-    clusters = dict.fromkeys(u.feature_name for u in inter_intent_data)
+    clusters = dict.fromkeys(u.feature_name for u in final_data)
     all_clusters = []
     train_clusters = []
+    dev_clusters = []
     test_clusters = []
     for c in clusters:
-        total = len([u for u in inter_intent_data if u.feature_name == c])
-        if c.endswith('_TRAIN'):
+        total = len([u for u in final_data if u.feature_name == c])
+        if c is None:
+            all_clusters.append((c, total))
+            dev_clusters.append((c, total))
+        elif c.endswith('_TRAIN'):
             all_clusters.append((c[:-6], total))
             train_clusters.append((c, total))
         elif c.endswith('_TEST'):
@@ -149,9 +161,10 @@ def get_train_test_data(generate_data=False):
 
     print('All ({}):'.format(len(all_clusters)), all_clusters)
     print('Train ({}):'.format(len(train_clusters)), train_clusters)
+    print('Dev ({}):'.format(len(dev_clusters)), dev_clusters)
     print('Test ({}):'.format(len(test_clusters)), test_clusters)
 
-    return inter_intent_data
+    return final_data
 
 
 def print_train_dev_test_stats(intent_data):
